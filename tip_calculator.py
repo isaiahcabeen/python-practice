e1e77d37835c20bee66102b44b9f21557866a8f7
Isaiah Cabeen <157175009+isaiahcabeen@users.noreply.github.com>
2025-11-26 15:08:54 -0600

    tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,5 +1,5 @@
-bill = int(input("What's the bill? "))
-tip_percentage = int(input("What percentage would you like to tip? "))
+bill = float(input("What's the bill? "))
+tip_percentage = float(input("What percentage would you like to tip? "))
 percent = tip_percentage / 100
 tip = bill * percent
 print(f"The total bill (including tip) is: {bill + tip}")
